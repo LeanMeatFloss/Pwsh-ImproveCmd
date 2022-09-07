c63c14d611775d3e78eb1daeb600af65db87fc53
Hansong Li <li-hans@outlook.com>
2022-09-07 10:41:14 +0800

    add Get-ChildItemImprove

--- PwshImproveCmd/PwshImproveCmd-Basic/Module/PwshImproveCmd-Basic.psm1
+++ PwshImproveCmd/PwshImproveCmd-Basic/Module/PwshImproveCmd-Basic.psm1
@@ -262,3 +262,23 @@ function Remove-ModulePathToEnv{
         }
     }
 }
+
+function Get-ChildItemImprove{
+    param(
+        [parameter(ValueFromPipeline)]
+        [string]
+        $Directory,
+        [scriptblock]
+        $FolderFilter={return $true},
+        [scriptblock]
+        $ReturnFilter={return $true}
+    )
+    process{
+        $subdirs=@()+(Get-ChildItem $Directory -Directory)
+        if($subdirs -and $subdirs.Count -gt 0){
+            $subdirs |Where-Object $FolderFilter|Get-ChildItemAdvance -FolderFilter $FolderFilter -ReturnFilter $ReturnFilter
+        }
+        # Get-ChildItem $Directory -Directory|Where-Object $FolderFilter|Get-ChildItemAdvance -FolderFilter $FolderFilter -ReturnFilter $ReturnFilter
+        Get-ChildItem $Directory -File|Where-Object $ReturnFilter
+    }
+}
